6dcc0c6728c6327da1f910e13689cd31851e0ed1
creator154 <cteator898@gmail.com>
2026-07-24 13:40:50 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,10 +1,13 @@
 import random
 
 from pyrogram import filters as Filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    Message
+)
 
 from ..translations import Messages as tr
-from ..config import Config
 from ..auth_store import auth_filter, add_broadcast_user
 from ..utubebot import UtubeBot
 
@@ -19,8 +22,36 @@ async def _start(c: UtubeBot, m: Message):
     add_broadcast_user(m.from_user.id)
 
     await m.reply_chat_action("typing")
+
+    caption = f"""
+<b>Hi there {m.from_user.mention} 🇮🇳.</b>
+
+I'm <b>YouTube Uploader Bot</b>. Made with ❤️ by @SumitTripathi.
+
+You can use me to upload any Telegram video to YouTube once you authorise me.
+
+📖 You can know more from /help.
+🔐 Or use /login to get started.
+"""
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("🔐 Login", callback_data="login"),
+                InlineKeyboardButton("📖 Help", callback_data="help")
+            ],
+            [
+                InlineKeyboardButton(
+                    "👨‍💻 Developer",
+                    url="https://t.me/HeySumit"
+                )
+            ]
+        ]
+    )
+
     await m.reply_photo(
         photo=random.choice(tr.IMAGE_LIST),
-        caption=tr.START_MSG.format(m.from_user.first_name),
-        quote=True,
+        caption=caption,
+        reply_markup=buttons,
+        quote=True
     )
