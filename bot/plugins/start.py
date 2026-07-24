bbf1b8b046a99c1e839c02d18ea13043b29dfdc7
creator154 <cteator898@gmail.com>
2026-07-24 15:37:51 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,6 +1,8 @@
-from pyrogram import filters
-from pyrogram.enums import ParseMode
+import random
+
+from pyrogram import filters as Filters
 from pyrogram.types import Message
+from pyrogram.enums import ParseMode
 
 from ..translations import Messages as tr
 from ..config import Config
@@ -8,16 +10,17 @@ from ..utubebot import UtubeBot
 
 
 @UtubeBot.on_message(
-    filters.private
-    & filters.incoming
-    & filters.command("start")
-    & filters.user(Config.AUTH_USERS)
+    Filters.private
+    & Filters.incoming
+    & Filters.command("start")
+    & Filters.user(Config.AUTH_USERS)
 )
 async def start(c: UtubeBot, m: Message):
 
     await m.reply_photo(
-        photo=tr.IMAGE,
-        has_spoiler=True,
+        photo=random.choice(tr.IMAGE_LIST),
         caption=tr.START_MSG.format(m.from_user.first_name),
-        parse_mode=ParseMode.HTML
-)
+        parse_mode=ParseMode.HTML,
+        has_spoiler=True,
+        quote=True
+    )
