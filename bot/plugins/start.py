d95fb7dd495fcdd16504099b73638116c6054966
creator154 <cteator898@gmail.com>
2026-07-24 14:20:44 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,44 +1,37 @@
 import random
 
-from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters as Filters
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    Message,
+)
 
 from ..translations import Messages as tr
 from ..utubebot import UtubeBot
 
 
-@UtubeBot.on_message(filters.private & filters.command("start"))
-async def start(_, m: Message):
-    caption = f"""
-<b>Hi there {m.from_user.mention} 👋</b>
-
-🎬 <b>Welcome to YouTube Uploader Bot</b>
-
-This bot can upload your Telegram videos directly to YouTube.
-
-📌 Commands:
-/login - Login with Google
-/help - Help
-/cancel - Cancel current task
-"""
+@UtubeBot.on_message(
+    Filters.private
+    & Filters.incoming
+    & Filters.command("start")
+)
+async def start(c: UtubeBot, m: Message):
 
     buttons = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton("🔐 Login", callback_data="login"),
-                InlineKeyboardButton("📖 Help", callback_data="help")
-            ],
-            [
-                InlineKeyboardButton(
-                    "🌐 Source",
-                    url="https://github.com/creator154/ZxutubeBot"
-                )
+                InlineKeyboardButton("📖 Help", callback_data="help"),
             ]
         ]
     )
 
+    await m.reply_chat_action("upload_photo")
+
     await m.reply_photo(
         photo=random.choice(tr.IMAGE_LIST),
-        caption=caption,
-        reply_markup=buttons
+        caption=tr.START_MSG.format(m.from_user.first_name),
+        reply_markup=buttons,
+        quote=True,
     )
