86e166d5d23a39238fbf4daf34040ae1f6f9299b
creator154 <cteator898@gmail.com>
2026-07-24 14:48:53 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,11 +1,7 @@
 import random
 
-from pyrogram import filters
-from pyrogram.types import (
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-    Message,
-)
+from pyrogram import filters as Filters
+from pyrogram.types import Message
 
 from ..translations import Messages as tr
 from ..config import Config
@@ -13,33 +9,17 @@ from ..utubebot import UtubeBot
 
 
 @UtubeBot.on_message(
-    filters.private
-    & filters.incoming
-    & filters.command("start")
-    & filters.user(Config.AUTH_USERS)
+    Filters.private
+    & Filters.incoming
+    & Filters.command("start")
+    & Filters.user(Config.AUTH_USERS)
 )
 async def start(c: UtubeBot, m: Message):
 
-    buttons = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🔐 Login",
-                    callback_data="login"
-                ),
-                InlineKeyboardButton(
-                    "📖 Help",
-                    callback_data="help"
-                ),
-            ]
-        ]
-    )
-
     await m.reply_photo(
         photo=random.choice(tr.IMAGE_LIST),
-        caption=tr.START_MSG.format(
-            m.from_user.mention
-        ),
+        caption=tr.START_MSG.format(m.from_user.first_name),
         has_spoiler=True,
-        reply_markup=buttons,
+        parse_mode="markdown",
+        quote=True,
     )
